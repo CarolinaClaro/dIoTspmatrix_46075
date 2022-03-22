8297fe58f0c2d4900e027f6fcbd77288df70acff
Carolina Claro <a.claro@campus.fct.unl.pt>
2022-03-22 17:57:01 +0000

    commit test

--- position.py
+++ position.py
@@ -1,1 +1,22 @@
+position = tuple[int,int]
+
+def position_create(row: int, col:int)->position:
+
+    if not (type(row) is int and row>=0) or not (type(col) is int and col>=0)
+        raise ValueError('position_create: invalid arguments')
+
+    return row, col
+
+def position_is(pos:position)->bool:
+
+
+def position_row(pos:position)->int:
+
+def position_col(pos:position)->int:
+
+def position_equal(pos1:position, pos2:position)->bool:
+
+def position_str(pos:position)->str:
+
+
 
