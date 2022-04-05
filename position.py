df8e3cff6f27da29ce9ae28a1643a2660a4cda59
Carolina Claro <a.claro@campus.fct.unl.pt>
2022-04-05 16:40:07 +0100

    pos e spmatrix update

--- position.py
+++ position.py
@@ -1,22 +1,35 @@
-position = tuple[int,int]
+from sympy import false, true
 
-def position_create(row: int, col:int)->position:
 
-    if not (type(row) is int and row>=0) or not (type(col) is int and col>=0)
+position = tuple[int, int]
+
+def position_create(row: int, col: int) -> position:
+    if not(type(row) is int and row >= 0) or not(type(col) is int and col >= 0):
         raise ValueError('position_create: invalid arguments')
-
     return row, col
 
-def position_is(pos:position)->bool:
+def position_is(pos: position) -> bool:
+    return type(pos) is tuple
 
+def position_row(pos: position) -> int:
+    row = pos[0]
+    if not(type(row) is int and row >= 0) or not(type(pos) is tuple):
+        raise ValueError('position_row: invalid arguments')
+    return row 
+    
+def position_col(pos: position) -> int:
+    col = pos[1]
+    if not(type(col) is int and col >= 0) or not(type(pos) is tuple):
+        raise ValueError('position_col: invalid arguments')
+    return col
 
-def position_row(pos:position)->int:
+def position_equal(pos1: position, pos2: position) -> bool:
+    if not(type(pos1) is tuple) or not(type(pos2) is tuple):
+        raise ValueError('position_equal: invalid arguments')
+    return pos1 == pos2
 
-def position_col(pos:position)->int:
-
-def position_equal(pos1:position, pos2:position)->bool:
-
-def position_str(pos:position)->str:
-
-
+def position_str(pos: position) -> str:
+    if not(type(pos) is tuple):
+        raise ValueError('position_str: invalid arguments')
+    return '(' + str(pos[0]) + ', ' + str(pos[1]) + ')'
 
--- spmatrix.py
+++ spmatrix.py
@@ -1,1 +1,112 @@
+from asyncio.windows_events import NULL
+from position import *
+
+spmatrix = list[float, dict] 
+
+def spmatrix_create(zero: float = 0.0) -> spmatrix:
+    if (type(zero) == int):
+        zero = float(zero)
+    if (not(type(zero) == float)):
+        raise ValueError('spmatrix_create: invalid arguments')
+    return list((zero, dict()))
+
+def spmatrix_is(mat: spmatrix) -> bool:
+    if (not(type(mat) == list)):
+       return False
+    else:
+        return True
+
+def spmatrix_zero_get(mat: spmatrix) -> float:
+    if (not(type(mat) == list)):
+        raise ValueError('spmatrix_zero_get: invalid arguments')
+    return mat[0]
+
+def spmatrix_zero_set(mat: spmatrix, zero: float):
+    if (type(zero) == int):
+        zero = float(zero)
+    if (not(type(mat) == list) or not(type(zero) == float)):
+        raise ValueError('spmatrix_zero_set: invalid arguments')
+
+    #set the matrix new zero value   
+    mat[0] = zero
+
+    #remove all items that contain the new zero
+    # temp_list = []
+    # i = 0
+
+    # for pos in mat[1]:
+    #     val = spmatrix_value_get(mat, pos)
+    #     if (val == zero):
+    #         temp_list[i] = val
+    #         i += 1
+    #         print(pos)
+    #         print('!!!!!!')
+    #         print(val)
+    # mat[1].pop(pos)         
+
+def spmatrix_value_get(mat: spmatrix, pos: position) -> float:
+    if (not(type(mat) == list) or not(position_is(pos) == True)):
+        raise ValueError('spmatrix_value_get: invalid arguments')
+    return mat[1].get(pos)
+
+def spmatrix_value_set(mat: spmatrix, pos: position, val: float):
+    if (type(val) == int):
+        val = float(val)
+    if (not(type(mat) == list) or not(position_is(pos) == True) or not(type(val) == float)):
+        raise ValueError('spmatrix_value_set: invalid arguments')
+    mat[1].update({pos:val})
+
+def spmatrix_copy(mat: spmatrix) -> spmatrix:
+    if (not(type(mat) == list)):
+        raise ValueError('spmatrix_copy: invalid arguments')
+    return mat
+
+def spmatrix_dim(mat: spmatrix) -> list([tuple[position, position], ()]):
+    pos = position
+    if (not(type(mat) == list)):
+        raise ValueError('spmatrix_dim: invalid arguments')
+    if(len(mat[1].items()) == 0):
+        return ()
+    else:
+        rowList = list()
+        colList = list()
+        for pos in mat[1].keys():
+            rowList.append(position_row(pos))
+            colList.append(position_col(pos))
+        minPos = position_create(min(rowList), min(colList))
+        maxPos = position_create(max(rowList), max(colList))
+        return minPos, maxPos
+
+def spmatrix_sparsity(mat: spmatrix) -> float:
+    if (not(type(mat) == list)):
+        raise ValueError('spmatrix_sparsity: invalid arguments')
+    dim = spmatrix_dim(mat)
+    nrExistingElem = len(mat[1])
+    totalElem = (position_row(dim[1])-position_row(dim[0])+1) * (position_col(dim[1])-position_col(dim[0])+1)
+    return nrExistingElem/totalElem
+  
+def spmatrix_str(mat: spmatrix, format: str) -> str:
+    if (not(type(mat) == list) or not(type(format) == str)):
+        raise ValueError('spmatrix_str: invalid arguments') 
+
+def spmatrix_row(mat: spmatrix, row: int) -> spmatrix:
+    if (not(type(mat) == list) or not(type(row) == int)):
+        raise ValueError('spmatrix_row: invalid arguments')
+
+def spmatrix_col(mat: spmatrix, col: int) -> spmatrix:
+    if (not(type(mat) == list) or not(type(col) == int)):
+        raise ValueError('spmatrix_col: invalid arguments')
+
+def spmatrix_diagonal(mat: spmatrix): 
+    if (not(type(mat) == list)):
+        raise ValueError('spmatrix_diagonal: invalid arguments')
+
+# mat = spmatrix_create()
+# spmatrix_value_set(mat, position_create(1,2), 12.5)
+# spmatrix_value_set(mat, position_create(2,1), 5.0)
+# spmatrix_value_set(mat, position_create(3,3), 8.0)
+# spmatrix_zero_set(mat, 8.0)
+# print(mat[0])
+# print(' ||| ')
+# print(mat[1])
 
